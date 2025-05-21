373f1223b51b49a5fd37e9e83142de18589e38ec
Dragonofvoid665 <abbaskarabas270@gmail.com>
2025-05-21 12:42:16 +0500

    Delete tkm directory

--- tkm/film/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse,HttpResponseRedirect
-from film.models import Product, Company
-
-
-def ActorsVeiw(request):
-    products = Company.objets.all()
-    return render(request,'test.html',{products:"products"})
