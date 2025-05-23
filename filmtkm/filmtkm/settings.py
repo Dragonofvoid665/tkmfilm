b524362e0185eede0bd873a42103664a59efdf2f
Dragonofvoid665 <abbaskarabas270@gmail.com>
2025-05-23 16:35:29 +0500

    Add files via upload

--- filmtkm/filmtkm/settings.py
+++ filmtkm/filmtkm/settings.py
@@ -139,5 +139,5 @@ JAZZMIN_SETTINGS = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2,  # количество объектов на одной странице
+    'PAGE_SIZE': 10,  # количество объектов на одной странице
 }
